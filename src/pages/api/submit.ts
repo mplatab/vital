e3c6d3d238eef651e67f6fc52db0cf152d0f79f2
mplatab <marcosplata98@gmail.com>
2025-01-06 21:37:54 -0500

    fix: first commit vital page

---
+++ src/pages/api/submit.ts
@@ -0,0 +1,47 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const formData = await request.json();
+    // Agregar timestamp y página
+    const timestamp = new Date().toLocaleString("es-EC", {
+      timeZone: "America/Guayaquil",
+    });
+    const data = {
+      ...formData,
+      timestamp,
+      landingPage: "pagina-principal",
+    };
+
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbxZLzsQOGr4ttwshKPjkp3LUkj5RYLaRdJIOGItirYjetBtN-3in3FFN__2WeMb73B7wg/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Error en la respuesta");
+    }
+
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Error desconocido";
+    return new Response(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+};
