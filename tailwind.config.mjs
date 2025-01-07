e99e2fab384f51b02bf03cc280f19111c38fcdc1
mplatab <marcosplata98@gmail.com>
2025-01-07 01:35:25 -0500

    update: image and code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,24 @@ export default {
 		  fontFamily: {
 			'milks': ['"TT Milks Script"', 'serif'],
 		  },
+		  colors: {
+			primary: {
+			  DEFAULT: '#FF6100',  // Naranja principal
+			  hover: '#E65600',    // Naranja hover
+			  light: '#FFB380',    // Naranja suave
+			},
+			complementary: {
+			  DEFAULT: '#004EFF',  // Azul intenso
+			  dark: '#00307D',     // Azul oscuro
+			},
+			neutral: {
+			  light: '#FFF5F0',    // Blanco cálido
+			  dark: '#1A0F00',     // Marrón muy oscuro
+			}
+		  },
+		  backgroundImage: {
+			'header': "url('/bg-header.webp')",
+		  },
 		},
 	},
 	plugins: [],
