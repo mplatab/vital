ff2f787475dbbb808a587d995d90231f6701a725
mplatab <marcosplata98@gmail.com>
2025-01-14 11:59:37 -0500

    updated deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
